81c20581f35ffcef6e5d2637cc8da9fd2b15beaa
ncor <toffuyume@gmail.com>
2024-07-13 19:05:42 +0300

    feat: replaced isEffect with effector's is.effect

--- src/core/helpers/toEffect.ts
+++ src/core/helpers/toEffect.ts
@@ -1,24 +1,14 @@
-import { createEffect, Effect } from "effector";
+import { createEffect, Effect, is } from "effector";
 import { MaybePromise } from "../../types";
-const EFFECT_KEYS = Object.keys(createEffect());
 
 export type EffectOrFn<Params, Done, Fail = Error> =
     | Effect<Params, Done, Fail>
     | ((params: Params) => MaybePromise<Done>);
 
-export function isEffect<Params, Done, Fail>(
-    effectOrFn: EffectOrFn<Params, Done, Fail>,
-): effectOrFn is Effect<Params, Done, Fail> {
-    return (
-        effectOrFn !== null &&
-        EFFECT_KEYS.every((key) => key in effectOrFn)
-    );
-}
-
 export function toEffect<Params, Done, Fail>(
     effectOrFn: EffectOrFn<Params, Done, Fail>,
 ): Effect<Params, Done, Fail> {
     return (
-        isEffect(effectOrFn) ? effectOrFn : createEffect(effectOrFn)
+        is.effect(effectOrFn) ? effectOrFn : createEffect(effectOrFn)
     ) as Effect<Params, Done, Fail>;
 }
