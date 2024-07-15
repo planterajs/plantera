19b2000ee9bbfe7740b3b4b6d5f38f5aeff963dd
ncor <toffuyume@gmail.com>
2024-07-15 07:59:41 +0300

    chore: updated example

--- test/examples/hello-world.ts
+++ test/examples/hello-world.ts
@@ -1,18 +1,18 @@
 import { controller, createRouter } from "../../src";
 import { createServer } from "node:http";
-import { RequestContext } from "../../src/context";
+import { RequestContext } from "../../src";
 
 const router = createRouter();
 createServer(router.callback).listen(3000);
 
-router
-    .get(
-        "/hello",
-        controller(() => "hello!"),
-    );
+router.get(
+    "/hello",
+    controller(() => "hello!"),
+);
 
-router
-    .get(
-        "/:name",
-        controller((context: RequestContext) => `hello, ${context.req.params.name}!`),
-    );
+router.get(
+    "/:name",
+    controller(
+        (context: RequestContext) => `hello, ${context.req.params.name}!`,
+    ),
+);
